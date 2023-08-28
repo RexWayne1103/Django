939d56cb5075ea13e1a6b3358bd089c8bf0eb9a6
nthu_BK <s.111064516@gapp.nthu.edu>
2023-08-28 21:29:30 +0800

    add first code

---
+++ djangodb/website/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Member
+def home(request):
+    all_members = Member.objects.all
+    return render(request, 'home.html', {'all':all_members})
+
+def join(request):
+    return render(request, 'join.html', {})
