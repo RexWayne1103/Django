ca176c266d7d54fa442370b4e1b55805f554cbe7
s.111064516 <s.111064516@gapp.nthu.edu.tw>
2023-08-29 00:47:59 +0800

    complete the course

--- djangodb/website/models.py
+++ djangodb/website/models.py
@@ -5,7 +5,7 @@ class Member(models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=100)
     
-    Email = models.EmailField(max_length=200)
+    email = models.EmailField(max_length=200)
     passwd = models.CharField(max_length=50)
     age = models.IntegerField()
 
--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Member
+from .forms import MemberForm
+from django.contrib import messages
+
 def home(request):
     all_members = Member.objects.all
     return render(request, 'home.html', {'all':all_members})
 
 def join(request):
-    return render(request, 'join.html', {})
+    if request.method == "POST" :
+        form = MemberForm(request.POST or None)
+        if form.is_valid(): ##檢查是否都有值
+            form.save()
+        else:
+            fname = request.POST['fname']
+            lname = request.POST['lname']
+            age = request.POST['age']
+            email = request.POST['email']
+            passwd = request.POST['passwd']
+
+            messages.success(request, ('There was an error in your form!Please try again...'))
+            return render(request, 'join.html', {
+                'fname':fname,
+                'lname':lname,
+                'age':age,
+                'email':email,
+                'passwd':passwd,
+            })
+            #return redirect('join')
+        messages.success(request, ('You form has bean submitted successfully!'))
+        return redirect('home')  
+
+    else:
+        return render(request, 'join.html', {})
